bcd94eb7e686cb35bc338ceb14e2f882c7bcb932
DefinitionCloud <42616995+DefinitionCloud@users.noreply.github.com>
2018-08-31 06:41:35 -0400

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -3,8 +3,8 @@
 # settings
 declare version='0.1'
 declare update=true
-declare source_dir=~/.osx-bootstrap
-declare source_file=$source_dir/.osx-bootstrap
+declare source_dir=~/OSX-Bootstrap
+#declare source_file=$source_dir/.osx-bootstrap
 
 # variables
-declare source_file_tmp=$source_dir/.osx-bootstrap-tmp
+#declare source_file_tmp=$source_dir/.osx-bootstrap-tmp
