2e3d306ba3799ddf7386713f3f9a0749061956b8
DefinitionCloud <42616995+DefinitionCloud@users.noreply.github.com>
2018-08-31 09:12:18 -0400

    Add files via upload

---
+++ system.sh
@@ -0,0 +1,304 @@
+#!/bin/bash
+
+# libs
+. settings.sh
+. functions.sh
+
+# variables
+declare workspace_dir=~/Workspace
+
+# clean
+clean
+
+# title
+echo ""
+echo "${bcyan}System"
+
+# setup workspace dir
+echo ""
+echo "${cyan}Creating folder structure...${reset} (Step: 1 of 3)"
+[[ ! -d $workspace_dir ]] && mkdir -p $workspace_dir
+
+# configuring osx
+echo ""
+echo "${cyan}Configuring OSX...${reset} (Step: 2 of 3)"
+
+
+#############################
+# General UI/UX
+#############################
+
+# # ensure FileVault is active
+# clean
+# `sudo fdesetup isactive`
+# if [[ $? != 0 ]]; then
+#     echo ''
+#     read -p "##### Do you want to enable Disk Encryption? [Yn]" -n 1 -r
+#     if [[ $REPLY =~ ^[Yy]$ ]]; then
+#         echo ''
+#         sudo fdesetup enable
+#     else
+#         echo ''
+#     fi
+# fi
+
+# Set computer name (as done via System Preferences → Sharing)
+hostname=$(whoami)
+sudo scutil --set ComputerName "$hostname"
+sudo scutil --set HostName "$hostname"
+sudo scutil --set LocalHostName "$hostname"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$hostname"
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Menu bar: hide icons
+# for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+#   defaults write "${domain}" dontAutoLoad -array \
+#     "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+#     "/System/Library/CoreServices/Menu Extras/Volume.menu" \
+#     "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+#     "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+#     "/System/Library/CoreServices/Menu Extras/User.menu"
+# done
+
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+#############################
+# Trackpad, mouse, keyboard, bluetooth accessories, and input
+#############################
+
+# Disable “natural” (Lion-style) scrolling
+# defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Set language and text formats
+# defaults write NSGlobalDomain AppleLanguages -array "en" "ru"
+# defaults write NSGlobalDomain AppleLocale -string "ru_RU@currency=RUB"
+# defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+# defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+# Accelerate cursor (fast key repeat rate)
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Disable auto-correct
+# defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Enable tap-to-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+
+#############################
+# Finder
+#############################
+
+# Finder: disable window animations and Get Info animations
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`, `Nlsv`
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Set $HOME as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+
+
+#############################
+# Dock & Mission Control
+#############################
+
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don’t use
+# the Dock to launch apps.
+defaults write com.apple.dock persistent-apps -array
+
+# Add applications to Dock
+clear
+APPS=(
+	Finder
+	Launchpad
+	Siri
+	System\ Preferences
+	App Store
+	Calendar
+	space
+	WhatsApp
+	TeamViewer
+	Skype
+	Slack
+	space
+	Google\ Chrome\ Canary
+	Google\ Chrome
+	Firefox
+	Safari
+	torBrowser
+	space
+	Sublime\ Text
+	Visual\ Studio\ Code
+	iTerm2
+	Terminal
+	space
+	FileZilla
+	SourceTree
+	qBittorrent
+	# Dropbox
+	# Google\ Drive
+	space
+	manager-osx #xampp
+    # Docker
+    # Vagrant
+    # VirtualBox
+    space
+    Spotify
+    VLC
+)
+
+# Add applications to Dock (including blank spaces)
+for app in "${APPS[@]}"
+do
+    if [[ $app = space ]]; then
+    	# create blank space to dock
+        defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
+        else
+    	# create dock element
+    	defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/$app.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+    fi
+done
+killall Dock
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Turn off dock icons magnification
+defaults write com.apple.dock magnification -boolean false
+
+#############################
+# Safari & WebKit
+#############################
+
+# Set Safari’s home page to `about:blank` for faster loading
+defaults write com.apple.Safari HomePage -string "about:blank"
+
+# Prevent Safari from opening ‘safe’ files automatically after downloading
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+# Hide Safari’s bookmarks bar by default
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Show status bar
+defaults write com.apple.Safari ShowStatusBar -bool true
+
+# Hide Safari’s sidebar in Top Sites
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+# Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+# Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+#############################
+# Spotlight
+#############################
+
+# Disable Spotlight indexing for any volume that gets mounted and has not yet
+# been indexed before.
+# Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+
+# Change indexing order and disable some file types
+defaults write com.apple.spotlight orderedItems -array \
+  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+  '{"enabled" = 1;"name" = "DIRECTORIES";}' \
+  '{"enabled" = 1;"name" = "PDF";}' \
+  '{"enabled" = 1;"name" = "FONTS";}' \
+  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+  '{"enabled" = 0;"name" = "MESSAGES";}' \
+  '{"enabled" = 0;"name" = "CONTACT";}' \
+  '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+  '{"enabled" = 0;"name" = "IMAGES";}' \
+  '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+  '{"enabled" = 0;"name" = "MUSIC";}' \
+  '{"enabled" = 0;"name" = "MOVIES";}' \
+  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+  '{"enabled" = 0;"name" = "SOURCE";}'
+
+# Make sure indexing is enabled for the main volume
+sudo mdutil -i on / > /dev/null
+
+# Rebuild the index from scratch
+sudo mdutil -E / > /dev/null
+
+#############################
+# Terminal & iTerm 2
+#############################
+
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+#############################
+# Kill affected applications
+#############################
+for app in "cfprefsd" "Dock" "Finder" "Safari"  "SystemUIServer" "iTerm"; do
+  killall "${app}" > /dev/null 2>&1 || true
+done
+
+# software update
+echo ""
+echo "${cyan}Running OSX Software Updates...${reset} (Step: 3 of 3)"
+sudo softwareupdate -i -a
+
